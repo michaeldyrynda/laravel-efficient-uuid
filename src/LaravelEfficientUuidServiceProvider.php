664b8bbe7af8932933d4b887fefd5d4ef7a25cb4
Michael Dyrynda <michael@dyrynda.com.au>
2019-05-31 10:49:18 +0930

    Remove unneeded Blueprint, swap to macro

--- src/LaravelEfficientUuidServiceProvider.php
+++ src/LaravelEfficientUuidServiceProvider.php
@@ -4,6 +4,7 @@ namespace Dyrynda\Database;
 
 use Dyrynda\Database\Connection\MySqlConnection;
 use Illuminate\Database\Connection;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelEfficientUuidServiceProvider extends ServiceProvider
@@ -29,6 +30,10 @@ class LaravelEfficientUuidServiceProvider extends ServiceProvider
     {
         Connection::resolverFor('mysql', function ($connection, $database, $prefix, $config) {
             return new MySqlConnection($connection, $database, $prefix, $config);
+        });
+
+        Blueprint::macro('efficientUuid', function ($column) {
+            $this->addColumn('efficientUuid', $column);
         });
     }
 }
