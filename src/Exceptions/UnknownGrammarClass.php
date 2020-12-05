4ae6c87209cbfe204bd844fc2732b3614771a576
Michael Dyrynda <michael@dyrynda.com.au>
2020-12-05 15:03:16 +1030

    cs

--- src/Exceptions/UnknownGrammarClass.php
+++ src/Exceptions/UnknownGrammarClass.php
@@ -1,13 +1,9 @@
 <?php
 
-
 namespace Dyrynda\Database\Exceptions;
-
 
 class UnknownGrammarClass extends \Exception
 {
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $message = 'Unknown grammar class, unable to define database type.';
 }
--- src/LaravelEfficientUuidServiceProvider.php
+++ src/LaravelEfficientUuidServiceProvider.php
@@ -2,58 +2,35 @@
 
 namespace Dyrynda\Database;
 
-use Illuminate\Database\Connection;
-use Illuminate\Database\Schema\ColumnDefinition;
-use Illuminate\Database\Schema\Grammars\Grammar;
 use Illuminate\Support\Fluent;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Schema\Blueprint;
-use Dyrynda\Database\Connection\MySqlConnection;
-use Dyrynda\Database\Connection\SQLiteConnection;
-use Dyrynda\Database\Connection\PostgresConnection;
+use Illuminate\Database\Schema\ColumnDefinition;
+use Illuminate\Database\Schema\Grammars\Grammar;
 use Dyrynda\Database\Exceptions\UnknownGrammarClass;
 
 class LaravelEfficientUuidServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-
         Grammar::macro('typeEfficientUuid', function (Fluent $column) {
-            $className = (new \ReflectionClass($this))->getShortName();
+            switch (class_basename(static::class)) {
+                case 'MySqlGrammar':
+                    return sprintf('binary(%d)', $column->length ?? 16);
 
-            if ($className === "MySqlGrammar") {
-                return sprintf('binary(%d)', $column->length ?? 16);
+                case 'PostgresGrammar':
+                    return 'bytea';
+
+                case 'SQLiteGrammar':
+                    return 'blob(256)';
+
+                default:
+                    throw new UnknownGrammarClass;
             }
-
-            if ($className === "PostgresGrammar") {
-                return 'bytea';
-            }
-
-            if ($className === "SQLiteGrammar") {
-                return 'blob(256)';
-            }
-
-            throw new UnknownGrammarClass();
         });
 
-
         Blueprint::macro('efficientUuid', function ($column): ColumnDefinition {
-            /** @var Blueprint $this */
+            /* @var Blueprint $this */
             return $this->addColumn('efficientUuid', $column);
         });
     }
